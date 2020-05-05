3d42440a030e483efde90734bfb81dc58d4e4251
Jitendra Singh <georgian.jitendra@gmail.com>
2020-05-06 04:52:24 +0530

    fix Chrome failed to start: exited abnormally on travis CI

--- src/test/java/theinternet/TestSteps.java
+++ src/test/java/theinternet/TestSteps.java
@@ -10,6 +10,7 @@ import org.junit.rules.Timeout;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
@@ -32,7 +33,9 @@ public class TestSteps {
         }
 
         if (driver == null) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
         }
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
